d7f6e6db54211ebaa935e8ee876c3fda11fbe260
Spider-Webs <dyko615@naver.com>
2022-06-20 23:21:31 +0900

    memberService
    비밀번호 찾기
    임시비밀번호를 발송하여, 발송과 동시에 DB에 암호화하여 변경된 비밀번호를 저장한다 . 임시비밀번호의경우 숫자와 영문대문자를 조합하여 10자로 만들었다.

--- src/main/java/bucket/list/service/Member/MemberService.java
+++ src/main/java/bucket/list/service/Member/MemberService.java
@@ -1,16 +1,23 @@
 package bucket.list.service.Member;
 
 import bucket.list.domain.Member;
+import bucket.list.dto.MailDto;
 import bucket.list.dto.OAuthAttributes;
 import bucket.list.dto.SecurityMember;
 import bucket.list.dto.SessionMember;
 import bucket.list.repository.Member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -31,18 +38,20 @@ public class MemberService implements UserDetailsService, OAuth2UserService<OAut
 
     private final MemberRepository memberRepository;
     private final HttpSession httpSession;
+    private final JavaMailSender javaMailSender;
+
 
     public Member saveMember(Member member){
-        memberExist(member);
+//        memberExist(member);
         return memberRepository.save(member);
     }
 
-    private void memberExist(Member member){ // 중복가입확인여부 메서드
-        Optional<Member> findMember = memberRepository.findByMemberId(member.getMemberId());
-        if(findMember!=null){
-            throw new IllegalStateException("이미 가입된 회원입니다");
-        }
-    }
+//    private void memberExist(Member member){ // 중복가입확인여부 메서드
+//        Optional<Member> findMember = memberRepository.findByMemberId(member.getMemberId());
+//        if(findMember!=null){
+//            throw new IllegalStateException("이미 가입된 회원입니다");
+//        }
+//    }
 
     //데이터 베이스에서 회원정보를 가져오는 UserDetailService인터페이스에 구현 메서드
     @Override
@@ -50,9 +59,7 @@ public class MemberService implements UserDetailsService, OAuth2UserService<OAut
 
         Optional<Member> member = memberRepository.findByMemberId(memberId);
 
-        if(member==null){
-            throw new UsernameNotFoundException(memberId);
-        }
+        if (!member.isPresent()) throw new UsernameNotFoundException("존재하지 않는 Id 입니다.");
 
         return new SecurityMember(member.get());
 
@@ -97,5 +104,60 @@ public class MemberService implements UserDetailsService, OAuth2UserService<OAut
 
 
         return memberRepository.save(member);
+    }
+    public void equalEmail(String memberEmail){
+        Optional<Member> byMemberEmail = memberRepository.findByMemberEmail(memberEmail);
+
+        if(byMemberEmail==null){
+            throw new IllegalStateException("가입되어있지않는 이메일입니다 다시 입력해주세요");
+        }
+    }
+
+    //임시비밀번호 생성 및 회원 비밀번호 변경
+    public MailDto changePassword(String memberEmail){
+        String tempPassword = getTempPassword();
+        MailDto dto = new MailDto();
+        dto.setAddress(memberEmail);
+        dto.setTitle("버킷리스트 임시비밀번호 발송 이메일입니다");
+        dto.setMessage("안녕하세요. 버킷리스트 임시비밀번호 안내 관련 이메일 입니다." + " 회원님의 임시 비밀번호는 "
+                + tempPassword + " 입니다." + "임시 비밀번호로 로그인 후에 비밀번호를 변경을 해주세요");
+        updatePassword(tempPassword,memberEmail);
+        return dto;
+    }
+    //임시비밀번호로 업데이트
+    public void updatePassword(String tempPassword, String memberEmail){
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String memberPassword = passwordEncoder.encode(tempPassword);
+        Long memberId = memberRepository.findByMemberEmail(memberEmail).get().getMemberIdx();
+        memberRepository.updatePassword(memberId,memberPassword);
+    }
+    //메일 발송
+    public void mailSend(MailDto mailDto) {
+        System.out.println("전송 완료!");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailDto.getAddress());
+        message.setSubject(mailDto.getTitle());
+        message.setText(mailDto.getMessage());
+        message.setFrom("dyko3786@gmail.com");
+        message.setReplyTo("dyko3786@gmail.com");
+        System.out.println("message"+message);
+        javaMailSender.send(message);
+    }
+
+
+    //랜덤함수로 임시비밀번호 구문 만들기
+    public String getTempPassword(){
+        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        String str = "";
+
+        // 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 구문을 작성함
+        int idx = 0;
+        for (int i = 0; i < 10; i++) {
+            idx = (int) (charSet.length * Math.random());
+            str += charSet[idx];
+        }
+        return str;
     }
 }
