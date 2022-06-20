ac9e76816336f3bdfa68455a92ec588aa93be1cf
Spider-Webs <dyko615@naver.com>
2022-06-20 23:24:43 +0900

    MailDto
    임시번호 발급을 위한 Dto객체 생성

---
+++ src/main/java/bucket/list/dto/MailDto.java
@@ -0,0 +1,17 @@
+package bucket.list.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//임시비밀번호 발송을위한 dto
+@Getter
+@Setter
+@NoArgsConstructor
+public class MailDto {
+
+    private String address;
+    private String title;
+    private String message;
+
+}
