45364cf058ed395e7d0266f7b2f199111d7d8ca3
Spider-Webs <dyko615@naver.com>
2022-06-26 01:53:28 +0900

    오탈자 및 글자수정

--- src/main/java/bucket/list/domain/Community.java
+++ src/main/java/bucket/list/domain/Community.java
@@ -5,7 +5,9 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
@@ -13,23 +15,25 @@ public class Community {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int communityid;            // 게시물 번호 (기본키값)
+    private Integer communityIdx;            // 게시물 번호 (기본키값)
+    private String communitySubject;
+    private String communityTag;
+    private String communityOption;
+    private String communityText;
+    private LocalDate communityDate;
+    private String communityFile;
+    private String communityWriter;
 
-    private String communitysubject;
-    @Column(name = "communitytag")
-    private String communitytag;        // 태그
-    private String communitykind;       // 종류
-    private String communitylocation;   // 위치,장소
-    private String communitycontent;     // 내용
-    private String communityprice;      // 비용
-    private LocalDate communitycreatedate;
-    private String communityfile;
-    private String communitylink;
-    private String communitywriter;
+    @Column(columnDefinition = "integer default 0", nullable = false)
+    private Integer communityCount;
+
+    @OneToMany(mappedBy = "community", cascade = CascadeType.REMOVE)
+    private List<CommunityComment> comments = new ArrayList<>();
+
 
     @PrePersist
     public void localAboutDate(){
-        this.communitycreatedate = LocalDate.now();
+        this.communityDate = LocalDate.now();
     }
 
 
