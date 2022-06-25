0a4ce0bd8c5d1a16a6ff71adde679a853ca2b90f
Spider-Webs <dyko615@naver.com>
2022-06-26 01:53:44 +0900

    커뮤니티 댓글 추가

---
+++ src/main/java/bucket/list/domain/CommunityComment.java
@@ -0,0 +1,29 @@
+package bucket.list.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+public class CommunityComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer commentIdx;
+
+    private String commentText;
+
+    @ManyToOne
+    @JoinColumn(name = "member_idx")
+    private Member member; // 작성자
+
+
+    @ManyToOne
+    @JoinColumn(name = "community_idx")
+    private Community community;
+
+    public void modifyComment(String commentText) {this.commentText = commentText;}
+}
