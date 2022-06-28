ec7f3d72357571ff9e019cdd981021db788f01fc
Spider-Webs <dyko615@naver.com>
2022-06-28 13:04:21 +0900

    고객센터 컨트롤러
    JPA 객체지향 설계로변경
    - 기존 게시글 작성자 writer 변수를 만들어, setter를 이용하여, 현재 세션아이디를 대입하였다.
    - 고객센터과, 유저간의 N:1관계로 변경

--- src/main/java/bucket/list/controller/CustomerController.java
+++ src/main/java/bucket/list/controller/CustomerController.java
@@ -1,11 +1,12 @@
 package bucket.list.controller;
 
 
-import bucket.list.domain.About;
+import bucket.list.config.LoginUser;
 import bucket.list.domain.Customer;
-import bucket.list.domain.Login;
+import bucket.list.dto.SessionMember;
 import bucket.list.service.Customer.CustomerService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,13 +14,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.io.IOException;
-
+@Slf4j
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
@@ -36,9 +37,7 @@ public class CustomerController {
     @GetMapping()
     public String customer(Model model,@PageableDefault(page = 0, size = 10, sort = "customerIdx",direction = Sort.Direction.DESC) Pageable pageable){
 
-
         Page<Customer> customer_items = customerService.allContentList(pageable);
-
 
         //현재 페이지 변수 Pageable 0페이지부터 시작하기 +1을해줘서 1페이지부터 반영한다
         int nowPage = customer_items.getPageable().getPageNumber() +1;
@@ -53,88 +52,51 @@ public class CustomerController {
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
 
-
         return "/customer/main";
     }
     //고객센터 글작성 보여주는 메서드
     @GetMapping("/write")
-    public String writeForm(){
+    public String writeForm(Model model){
+
+        model.addAttribute("customer", new Customer());
+
         return "/customer/write";
     }
     //고객센터 글작성폼에서 넘어오는 메서드
     @PostMapping("/write")
-    public String writeForm(HttpServletRequest request, @ModelAttribute("customer") Customer customer, MultipartFile file) throws IOException {
+    public String writeForm(@Valid @ModelAttribute Customer customer,BindingResult bindingResult,@LoginUser SessionMember sessionMember, MultipartFile file) throws IOException {
 
+        if(bindingResult.hasErrors()){
 
-        HttpSession session = request.getSession();
-        if(session != null){
-            customer.setCustomerWriter(((Login)session.getAttribute("loginMember")).getId());
-            customerService.Save(customer, file);
+            return "customer/write";
         }
-
-
+        customerService.save(customer, file,sessionMember.getMemberId());
         return "redirect:/customer";
     }
 
-    //고객센터 하나의 게시글보여주고 비밀글여부확인
+    //고객센터 게시글 클릭 후 비밀번호 페이지 이동
     @GetMapping("/{customerIdx}/read")
-    public String readOrSecretRead(@PathVariable int customerIdx, Model model,HttpServletRequest request){
+    public String readOrSecretRead(@PathVariable int customerIdx, Model model){
 
-        HttpSession session = request.getSession();
-        String sessionwriter = ((Login)session.getAttribute("loginMember")).getId();   // sessionwriter에 현재 세션의 user이름을 넣어줌
-        String dbwriter = customerService.selectIdSQL(customerIdx);       // 해당 게시물의 writer 정보를 dbwriter에 저장함
+        model.addAttribute("customerIdx", customerIdx);
+        return "/customer/secretRead";
 
-
-        Customer customer = customerService.oneContentList(customerIdx);
-
-        if(customer.getCustomerSecret().equals("f")){
-            model.addAttribute("customer", customer);
-            return "/customer/secretread";
-        }else {
-
-            if(sessionwriter.equals(dbwriter)){
-                model.addAttribute("dbwriter", dbwriter);
-                model.addAttribute("customer",customer);
-
-                return "/customer/read";
-            }else{
-                model.addAttribute("customer",customer);
-
-                return "/customer/read";
-            }
-
-        }
     }
     //비밀글에서 비밀번호 입력
     @PostMapping("/{customerIdx}/read")
     public String secretReadConfirm(@RequestParam("customerPassword")String customerPassword,
-                                    @RequestParam("customerIdx")int customerIdx,Model model,
-                                    HttpServletRequest request){
-
-        HttpSession session = request.getSession();
-        String sessionwriter = ((Login)session.getAttribute("loginMember")).getId();   // sessionwriter에 현재 세션의 user이름을 넣어줌
-        String dbwriter = customerService.selectIdSQL(customerIdx);       // 해당 게시물의 writer 정보를 dbwriter에 저장함
+                                    @PathVariable("customerIdx")int customerIdx,Model model,
+                                    @LoginUser SessionMember sessionMember){
 
         Customer customer = customerService.oneContentList(customerIdx);
 
         if(customer.getCustomerPassword().equals(customerPassword)){
-
-            if(sessionwriter.equals(dbwriter)){
-                model.addAttribute("dbwriter", dbwriter);
-                model.addAttribute("customer",customer);
-
-                return "/customer/read";
-            }else{
-                model.addAttribute("customer",customer);
-
-                return "/customer/read";
-            }
-
-
+            log.info("customerId = {},sessionId = {}",customer.getMember().getMemberId(),sessionMember.getMemberId());
+            model.addAttribute("sessionMember", sessionMember.getMemberId());
+            model.addAttribute("customer", customer);
+            return "/customer/read";
         }else{
-
-            model.addAttribute("customerIdx", customerIdx);
-            return "/customer/readfail";
+            return "customer/readFail";
         }
     }
 
@@ -149,21 +111,17 @@ public class CustomerController {
 
     @PostMapping("/edit/{customerIdx}")
     //실제 게시글수정, 파일이미지 업로드
-    public String edit(@ModelAttribute("customer") Customer customer, @PathVariable int customerIdx, MultipartFile file) throws IOException {
-        customerService.Save(customer,file);
+    public String edit(@ModelAttribute("customer") Customer customer, @PathVariable int customerIdx, MultipartFile file,@LoginUser SessionMember sessionMember) throws IOException {
+        customerService.save(customer,file,sessionMember.getMemberId());
         return "redirect:/customer/";
     }
-
 
     //고객센터 글삭제 컨트롤러
     //게시글 삭제
     @GetMapping("/delete/{customerIdx}")
     public String delete(@PathVariable int customerIdx){
         customerService.deleteContent(customerIdx);
-
         return "redirect:/customer";
     }
-
-
 
 }
