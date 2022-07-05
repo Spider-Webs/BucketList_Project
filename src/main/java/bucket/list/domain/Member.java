3737e3cfbfd8635bdb7515e9330015a5f3369fbc
Spider-Webs <dyko615@naver.com>
2022-07-05 14:18:28 +0900

    인덱스추가

--- src/main/java/bucket/list/domain/Member.java
+++ src/main/java/bucket/list/domain/Member.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @Setter
 @Getter
 @Entity
+@Table(indexes = @Index(name = "member_index", columnList = "member_name"))
 public class Member implements Serializable {
 
     @Id
