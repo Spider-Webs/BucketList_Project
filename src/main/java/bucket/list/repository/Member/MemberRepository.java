daa6204f58cf9b297563c487c8e1f42d6a931078
Spider-Webs <dyko615@naver.com>
2022-07-05 14:18:40 +0900

    인덱스추가

--- src/main/java/bucket/list/repository/Member/MemberRepository.java
+++ src/main/java/bucket/list/repository/Member/MemberRepository.java
@@ -1,11 +1,13 @@
 package bucket.list.repository.Member;
 
 import bucket.list.domain.Member;
+import bucket.list.domain.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,5 +22,7 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     @Query(value = "update Member m set m.memberPassword = :memberPassword where m.memberIdx = :memberIdx")
     void tempUpdatePassword(Long memberIdx, String memberPassword);
 
+    @Query("select m from Member m where  m.memberName like %:memberKeyword%  order by m.memberIdx desc")
+    List<Member> findByMemberNameContaining(String memberKeyword);
 
 }
