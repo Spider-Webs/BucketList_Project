87bea8b01b0e5c42ed167ab9dea1396751d4f896
Spider-Webs <105139722+Spider-Webs@users.noreply.github.com>
2022-09-02 22:33:35 +0900

    Update CustomAuthenticationEntryPoint.java

--- src/main/java/bucket/list/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/bucket/list/config/CustomAuthenticationEntryPoint.java
@@ -3,7 +3,7 @@ package bucket.list.config;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import javax.servlet.ServletException;
+import javax.servlet.ServletException; 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
